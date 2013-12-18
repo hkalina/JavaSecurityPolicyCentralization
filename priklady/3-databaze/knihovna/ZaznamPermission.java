0f994bc332b0786140b3c530e465ba62a9e8ad25
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2013-12-19 00:24:47 +0100

    doc

--- priklady/3-databaze/knihovna/ZaznamPermission.java
+++ priklady/3-databaze/knihovna/ZaznamPermission.java
@@ -1,14 +1,8 @@
 import java.security.BasicPermission;
 
-// musí být public!
+// třída oprávnění musí být public
 public class ZaznamPermission extends BasicPermission {
-    
-    public ZaznamPermission(String name) {
-        super(name);
-    }
-    
     public ZaznamPermission(String name, String actions) {
         super(name, actions);
     }
-    
 }
